9ea0ff8ffe186066e2dfe7391b58d4dffedd946e
Arthguilherme <arth.ssg@gmail.com>
2026-08-16 09:40:04 -0300

    feat: parte um da atividade concluida

--- backend/routes/cepRoutes.js
+++ backend/routes/cepRoutes.js
@@ -26,6 +26,29 @@ router.get('/cep/:cep', async (req, res) => {
 router.get('/endereco/:uf/:cidade/:logradouro', async (req, res) => {
     const { uf, cidade, logradouro } = req.params;
 
-})
+    if (cidade.length < 3 || logradouro.length < 3) {
+        return res.status(400).json({
+            erro: "Cidade e Logradouro devem ter no mínimo 3 caracteres."
+        });
+    }
+
+    try {
+        const resposta = await fetch(`https://viacep.com.br/ws/${uf}/${cidade}/${logradouro}/json/`);
+        const dados = await resposta.json();
+
+        if (dados.length === 0) {
+            return res.status(404).json({
+                erro: "Nenhum endereço encontrado para esses parâmetros."
+            });
+        }
+
+        res.status(200).json(dados);
+
+    } catch (err) {
+        res.status(500).json({
+            erro: "Erro de comunicação com VIACEP"
+        });
+    }
+});
 
 module.exports = router;
