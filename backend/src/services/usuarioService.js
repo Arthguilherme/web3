4eb38a488388f451cf0f9a464acf9695a2a79e98
Arthguilherme <arth.ssg@gmail.com>
2026-09-01 20:37:20 -0300

    feat: feito em sala na ultima aula

--- backend/src/services/usuarioService.js
+++ backend/src/services/usuarioService.js
@@ -1,15 +1,7 @@
+const Usuario = require('../models/Usuario');
 
 const obterTodosUsuarios = async () => {
-    const mockUsuarios = [
-        {
-            id: 1, nome: 'João', email: 'joao@email.com'
-        },
-        {
-            id: 2, nome: 'Marcos' , email: 'marcos@email.com'
-        }
-    ];
-
-    return mockUsuarios;
+   return await Usuario.findAll ();
 };
 
 module.exports = { obterTodosUsuarios }
