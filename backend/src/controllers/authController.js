65ff57776343e90ab034960bae7ee13c1b123c76
Arthguilherme <arth.ssg@gmail.com>
2026-09-25 16:15:05 -0300

    feat: rotas e auth configurados

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,19 @@
+const authService = require('../services/authService');
+
+const login = async (req, res) => {
+    try {
+        const { email, senha } = req.body;
+        const resultado = await authService.login(email, senha);
+
+        res.status(200).json({ data: resultado });
+
+    } catch (err) {
+        if (err.message === 'CREDENCIAIS_INVALIDAS') {
+            return res.status(401).json({ err: 'Email ou senha inválidos' });
+        }
+
+        res.status(500).json({ err: 'Erro interno ao fazer login' });
+    }
+};
+
+module.exports = { login };
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const usuarioRoutes = require('./usuarioRoutes');
+const authRoutes = require('./autRoutes');
 
 const router = express.Router();
 
 router.use('/usuarios', usuarioRoutes );
-
+router.use('/', authRoutes);
 
 module.exports = router;
