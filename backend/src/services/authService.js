8fe47f2da15bdcfdfaaa7ff850eb1ef1593061bf
Arthguilherme <arth.ssg@gmail.com>
2026-09-25 15:49:45 -0300

    feat: autservice completo

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -2,10 +2,27 @@ const Usuario = require('../models/Usuario');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const login = async (email, senha) => {
-    const usuario = await Usuarioo.scope('comSenha').findOne({ where: { email } });
+    const login = async (email, senha) => {
+        const usuario = await Usuarioo.scope('comSenha').findOne({ where: { email } });
 
-    if (!usuario) {
+        if (!usuario) {
+          throw new Error('Credenciais Inválidas');
+        }
+    
+    const confere = await bcrypt.compare(senha, usuario.senha);
+
+    if (!confere) {
         throw new Error('Credenciais Inválidas');
     }
-}
+
+    const token = jwt.sign(
+        { id: usuario.id, nome: usuario.nome },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    return { token, usuario: { id: usuario.id, nome: usuario.nome, email:usuario.email } };
+
+};
+
+module.exports = { login };
